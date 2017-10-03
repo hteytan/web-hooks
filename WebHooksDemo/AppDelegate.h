b9fbd6618c0d667598a9adcef6d7ca031fae633d
Eytan Moudahi <eytan.moudahi@healthtap.com>
2017-10-03 16:27:06 -0700

    resolve top level blah blah

---
+++ WebHooksDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebHooksDemo
+//
+//  Created by Eytan Moudahi on 10/3/17.
+//  Copyright © 2017 Eytan Moudahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
